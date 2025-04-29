129869734aa8bbc990c02d1160958967c5b422a3
mohammad-kadham <mohammadkadham61@gmail.com>
2025-04-30 01:20:40 +0300

    added fetchProducts

--- controller/shop.js
+++ controller/shop.js
@@ -1,21 +1,27 @@
 const Product = require('../model/addProduct')
 const fs = require('fs');
-module.exports.getMainPage=(req,res)=>{
-    res.render('index',{path:'/',pageTitle:'shop'})
+module.exports.getMainPage = (req, res) => {
+  Product.fetchAllProduct((products) => {
+    console.log(products);
+    res.render('index', { path: '/', pageTitle: 'shop', products: products })
+  })
+
+
+
 }
 
-module.exports.getAddProduct=(req,res)=>{
-    res.render('add-product',{path:'/add-product',pageTitle:'add your product'})
+module.exports.getAddProduct = (req, res) => {
+  res.render('add-product', { path: '/add-product', pageTitle: 'add your product' })
 }
-module.exports.postAddProduct=(req,res)=>{
-  const {title,description,price} = req.body
-    const product = new Product('dgetdf',title,description,price)
-    product.saveProduct()
+module.exports.postAddProduct = (req, res) => {
+  const { title, description, price } = req.body
+  const product = new Product('dgetdf', title, description, price)
+  product.saveProduct()
 
 
   console.log('--------');
-  
-    res.redirect('/')
+
+  res.redirect('/')
 }
 
 
--- model/addProduct.js
+++ model/addProduct.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
-
+const pathOfFile = './output.txt'
+const path = require('path')
 class Product {
-    static productArray =[]
+
     constructor(id, productName, productDes, price) {
         this.id = id;
         this.productName = productName;
@@ -10,19 +11,52 @@ class Product {
     }
 
     saveProduct() {
-        Product.productArray.push(this)
-       const jsonData =  JSON.stringify([...Product.productArray])
-       console.log(jsonData);
-       
-        fs.writeFile('output.txt', jsonData, 'utf8', (err) => {
+        this.readFileFn(pathOfFile, (err, data) => {
+            if (!err) {
+                const productArray = JSON.parse(data)
+                productArray.push(this)
+                const dataString = JSON.stringify(productArray)
+                fs.writeFile(pathOfFile, dataString, (err) => {
+                    if (!err) {
+                        console.log('file written');
+
+                    }
+                })
+
+            }
+        })
+
+        // const jsonData = JSON.stringify([...Product.productArray])
+        // console.log(jsonData);
+
+        // fs.writeFile('output.txt', jsonData, 'utf8', (err) => {
+        //     if (err) {
+        //         console.error('Error writing to file:', err);
+        //         return;
+        //     }
+        //     console.log('File has been written');
+
+        // });
+
+    }
+   static readFileFn(path, cb) {
+        fs.readFile(path, 'utf-8', (err, data) => {
             if (err) {
-                console.error('Error writing to file:', err);
+                console.error('didnt read file')
+                cb(err, null)
                 return;
             }
-            console.log('File has been written');
-           
-        });
+            cb(null, data)
+        })
+    }
+   static fetchAllProduct(cb) {
+        this.readFileFn(pathOfFile, (err, data) => {
+            if (!err) {
+                const products = JSON.parse(data)
+                cb( products)
+            }
 
+        })
     }
 }
 
